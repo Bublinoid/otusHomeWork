a9b3e659eeb9f1d74bb92c8c9bdcc384c111c777
Hleb <gleb.qb@gmail.com>
2023-11-30 10:03:55 +0100

    home work 6 is ready

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,21 @@
 public class Main {
     public static void main(String[] args) {
 
+        Plate plate = new Plate(100, 40);
+        Cat[] cats = {
+                new Cat("Barsik", 10),
+                new Cat("Murzik", 15),
+                new Cat("Smooky", 20),
+        };
+
+        for (Cat cat : cats) {
+            cat.eat(plate);
+            cat.info();
+        }
+
+        plate.info();
+        plate.addFood(85);
+        plate.info();
     }
 }
 
