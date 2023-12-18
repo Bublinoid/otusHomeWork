503f6bd9d3c7c75da8d1387b32bf7ae4f289ff9b
Hleb <gleb.qb@gmail.com>
2023-12-18 08:02:47 +0100

    first change

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,15 @@
 public class Main {
     public static void main(String[] args) {
 
+        new Thread(() -> {
+            Server server = new Server();
+            server.start();
+        }).start();
+
+        Client tom = new Client("tom");
+        tom.start();
+        tom.sendPersonalMessage("tom", "Hello");
+
+        tom.disconnect();
     }
 }
-
